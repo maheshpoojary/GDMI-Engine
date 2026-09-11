5f7d772973c297c83f8355edc3c3d7c6c5592177
maheshpoojary <68710560+maheshpoojary@users.noreply.github.com>
2026-09-11 15:15:19 +0530

    Update GDMIECore.kt

--- GDMIECore.kt
+++ GDMIECore.kt
@@ -1,1 +1,12 @@
+package com.mahesh.gdmi.engine
 
+class GDMIECore {
+
+    fun process(input: Map<String, Double>): Double {
+        if (input.isEmpty()) {
+            return 0.0
+        }
+
+        return input.values.average()
+    }
+}
