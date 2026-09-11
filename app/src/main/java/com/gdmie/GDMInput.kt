8a3082ffc1b8615c8c784e5523b65124cebf218e
maheshpoojary <68710560+maheshpoojary@users.noreply.github.com>
2026-09-11 23:16:12 +0530

    Update GDMInput.kt

--- app/src/main/java/com/gdmie/GDMInput.kt
+++ app/src/main/java/com/gdmie/GDMInput.kt
@@ -1,1 +1,11 @@
+package com.gdmie
 
+data class GDMInput(
+    val presentValue: Double,
+    val recentValue: Double,
+    val expectedValue: Double,
+    val marketValue: Double,
+    val contextFactor: Double,
+    val momentumFactor: Double,
+    val riskFactor: Double
+)
