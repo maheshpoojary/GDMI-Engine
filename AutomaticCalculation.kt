f0c838b8401ed1ac2b49e773a33030392d79ab61
maheshpoojary <68710560+maheshpoojary@users.noreply.github.com>
2026-09-11 15:30:52 +0530

    Update AutomaticCalculation.kt

--- AutomaticCalculation.kt
+++ AutomaticCalculation.kt
@@ -1,1 +1,21 @@
+package com.mahesh.gdmi.engine
 
+class AutomaticCalculation {
+
+    fun calculate(input: Map<String, Double>): Double {
+
+        if (input.isEmpty()) {
+            return 0.0
+        }
+
+        return input.values.average()
+    }
+
+    fun calculateSum(input: Map<String, Double>): Double {
+        return input.values.sum()
+    }
+
+    fun calculateCount(input: Map<String, Double>): Int {
+        return input.size
+    }
+}
