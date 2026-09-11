84e4b20fe1e0ab4b389e1d6c7fa1680b78a15b45
maheshpoojary <68710560+maheshpoojary@users.noreply.github.com>
2026-09-11 15:22:57 +0530

    Update DataInput.kt

--- engine/DataInput.kt
+++ engine/DataInput.kt
@@ -1,1 +1,34 @@
+package com.mahesh.gdmi.engine
 
+class DataInput {
+
+    private val data = mutableMapOf<String, Double>()
+
+    fun add(key: String, value: Double) {
+        data[key] = value
+    }
+
+    fun get(key: String): Double? {
+        return data[key]
+    }
+
+    fun getAll(): Map<String, Double> {
+        return data.toMap()
+    }
+
+    fun remove(key: String) {
+        data.remove(key)
+    }
+
+    fun clear() {
+        data.clear()
+    }
+
+    fun isEmpty(): Boolean {
+        return data.isEmpty()
+    }
+
+    fun size(): Int {
+        return data.size
+    }
+}
