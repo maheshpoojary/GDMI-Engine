0ce6550988679afd30bc2f4f01378f3244451548
maheshpoojary <68710560+maheshpoojary@users.noreply.github.com>
2026-09-11 13:20:03 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.mahesh.gdmi"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mahesh.gdmi"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
