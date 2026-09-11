ec12deca522f791f94ef41f1d4bf0aa76c7f90c7
maheshpoojary <68710560+maheshpoojary@users.noreply.github.com>
2026-09-11 19:12:17 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,9 @@
 pluginManagement {
+    plugins {
+        id("com.android.application") version "8.6.1"
+        id("org.jetbrains.kotlin.android") version "1.9.25"
+    }
+
     repositories {
         google()
         mavenCentral()
