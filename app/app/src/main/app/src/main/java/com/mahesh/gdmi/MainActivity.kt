e178f08c1807d0ff9535afeb816c83a656cb4c10
maheshpoojary <68710560+maheshpoojary@users.noreply.github.com>
2026-09-11 13:48:42 +0530

    Update MainActivity.kt

--- app/app/src/main/app/src/main/java/com/mahesh/gdmi/MainActivity.kt
+++ app/app/src/main/app/src/main/java/com/mahesh/gdmi/MainActivity.kt
@@ -1,1 +1,17 @@
+package com.mahesh.gdmi
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.mahesh.gdmi.engine.ReverseEdgeScreen
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ReverseEdgeScreen()
+        }
+    }
+}
