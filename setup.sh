82555704409c7b88336d7bfdad25abec56f15dc8
Mahesh Poojary <Maheshpbk2@gmail.com>
2026-09-13 09:26:25 +0530

    Add Termux Android setup configuration

---
+++ setup.sh
@@ -0,0 +1,77 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# GitHub Action termux/termux-docker possible errors to be prevented:
+# + tar -xJf file.tar.xz
+# tar: Unknown option Jf (see "tar --help")
+# and
+# + tar -xf -
+# tar: chown ...: Operation not permitted
+# use
+# xz -d file.tar.xz
+# gzip -d file.tar.gz
+# tar -xf file.tar || true
+
+: "${PROFILE:=${HOME}/.bashrc}"
+cd ~ || exit
+if [ "$PROFILE" != '/dev/null' ]; then
+  cat >>"$PROFILE" <<'EOF'
+
+export JAVA_HOME="$PREFIX/lib/jvm/java-21-openjdk"
+export ANDROID_HOME="${HOME}/Android/Sdk"
+export ANDROID_SDK_ROOT="${ANDROID_HOME}"
+export ANDROID_NDK_HOME="${HOME}/Android/Sdk/ndk/android-ndk-r29"
+export ANDROID_NDK_TOOLCHAINS="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-aarch64"
+export PATH="${PATH}:${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin"
+EOF
+else
+  # shellcheck disable=2016
+  echo '
+export JAVA_HOME="$PREFIX/lib/jvm/java-21-openjdk"
+export ANDROID_HOME="${HOME}/Android/Sdk"
+export ANDROID_SDK_ROOT="${ANDROID_HOME}"
+export ANDROID_NDK_HOME="${HOME}/Android/Sdk/ndk/android-ndk-r29"
+export ANDROID_NDK_TOOLCHAINS="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-aarch64"
+export PATH="${PATH}:${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin"
+'
+fi
+export JAVA_HOME="$PREFIX/lib/jvm/java-21-openjdk"
+export ANDROID_HOME="${HOME}/Android/Sdk"
+export ANDROID_SDK_ROOT="${ANDROID_HOME}"
+export ANDROID_NDK_HOME="${HOME}/Android/Sdk/ndk/android-ndk-r29"
+export ANDROID_NDK_TOOLCHAINS="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-aarch64"
+export PATH="${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin:${PATH}"
+pkg update
+pkg install aapt aapt2 aidl android-tools apksigner d8 jq openjdk-21 unzip wget -y
+wget --tries=100 --retry-connrefused --waitretry=5 -O studio.html https://developer.android.com/studio
+# shellcheck disable=2155
+export CMDLINETOOLS="$(awk '/<table class="download">/ { count++ }
+count >= 2 {
+  if (match($0, /commandlinetools-linux-.*zip/)) {
+    print substr($0, RSTART, RLENGTH)
+    exit
+  }
+}' studio.html)"
+rm studio.html*
+wget --tries=100 --retry-connrefused --waitretry=5 "https://dl.google.com/android/repository/${CMDLINETOOLS}"
+unzip "$CMDLINETOOLS"
+mkdir -p ~/Android/Sdk/cmdline-tools/latest
+mv cmdline-tools/* ~/Android/Sdk/cmdline-tools/latest
+rm -r cmdline-tools
+rm "$CMDLINETOOLS"*
+cd ~/Android/Sdk/cmdline-tools/latest/bin || exit
+for f in *; do
+  test -f "$f" && termux-fix-shebang "$f"
+done
+if [ "$#" -gt 0 ]; then
+  echo y | ./sdkmanager "$@"
+fi
+cd ~ || exit
+wget --tries=100 --retry-connrefused --waitretry=5 https://github.com/lzhiyong/termux-ndk/releases/download/android-ndk/android-ndk-r29-aarch64.tar.xz
+xz -d android-ndk-r29-aarch64.tar.xz
+tar -xf android-ndk-r29-aarch64.tar || true
+rm android-ndk-r29-aarch64.tar*
+mv android-ndk-r29 ~/Android/Sdk/ndk/
+mkdir -p ~/.gradle
+cat >~/.gradle/gradle.properties <<'EOF'
+android.aapt2FromMavenOverride=/data/data/com.termux/files/usr/bin/aapt2
+EOF
